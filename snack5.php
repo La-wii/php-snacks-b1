0ed605d05e63f9c25b95b6d967c95fe45fa6fdaa
La-wii <sentilavale@gmail.com>
2021-07-27 12:39:21 +0200

    snack 5 explode

---
+++ snack5.php
@@ -0,0 +1,21 @@
+<?php 
+    $paragrafo = 'Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+    Dopo con un ciclo stampare tutti i paragrafi ottenuti.';
+
+    $paragrafo_diviso=explode('.', $paragrafo);
+
+    var_dump($paragrafo_diviso);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+</body>
+</html>

21d1a4cd8169c7b4c9f76b1c7ea164534465b1a3
La-wii <sentilavale@gmail.com>
2021-07-28 20:00:05 +0200

    snack

--- snack6.php
+++ snack6.php
@@ -1,5 +1,19 @@
+<?php
 
+    $stringa = $_GET['password'];
+    
+    if (empty($_GET['password'])) {
+        echo 'Errore';
+    } else {
+        $stringa == $_GET['password'];
+        if ($stringa == 'Boolean') {
+            $class = 'green';
+        } else {
+            $class = 'red';
+        }
+    }
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,14 +23,20 @@
     <title>Document</title>
 </head>
 <body>
+
+    <style>
+        .green{
+            color: green;
+        }
+
+        .red{
+            color: red;
+        }
+    </style>    
     <p>
-        <?php
-            if ($password=$_GET['password'] == 'Boolean'){
-                echo $message_green = 'ok verde';
-            } else {
-                echo $message_red = 'no rosso';
-            }
-        ?>
+        
+        <h1 class="<?php echo $class ?>"> La parola é <?php echo $stringa ?></h1>
+        
     </p>
 </body>
 </html>
